1fc0986b7779902a2ae402def4bef6942e8127db
BySergy-Uny <bysergy.uny@gmail.com>
2022-10-23 21:03:30 +0200

    Search to API with keyword

--- Web-app/Tools/connection.py
+++ Web-app/Tools/connection.py
@@ -1,10 +1,15 @@
+from urllib.parse import urlencode
 import requests
+import re
 
 
-
-def get_request():
+def get_request(options):
     url = 'https://datos.gob.es/apidata/catalog/dataset'
     headers = {'Accept': 'application/json'}
-    return requests.get(url, headers=headers).content
- 
-print (get_request())
+    return requests.get(url + options, headers=headers).content
+
+def search_datasets_by_keyword(keyword):
+    options = "/keyword/" + keyword
+    return get_request(options)
+
+print (search_datasets_by_keyword("telefonia"))
